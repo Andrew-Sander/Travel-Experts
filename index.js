597a62ec1e46f3a7b7665fada043e6d912326580
Andrew Sander <andrsand1@gmail.com>
2023-06-07 16:33:34 -0600

    message

--- index.js
+++ index.js
@@ -55,14 +55,44 @@ app.get("/contact", (req, res) => {
 app.get('/register', (req, res) => {
     res.render('pages/register');
 });
-app.get('/thanks', (req, res) => {
-    res.render('pages/thanks');
+app.get('/welcome', (req, res) => {
+    res.render('pages/welcome');
 });
-
-// app.post("/create-post", (req, res) => {
-//     console.log(req.body.name + " " + req.body.phone + " " + req.body.message);
-//     res.redirect('thanks');
-// });
+app.get('/login', (req, res) => {
+    res.render('pages/login');
+});
+app.post("/register", (req, res) => {
+    console.log(req.body.fname + ", " + req.body.phone);
+    res.redirect('/welcome');
+});
+// http://localhost:3000/auth
+app.post('/login', function(request, response) {
+	// Capture the input fields
+	let username = request.body.username;
+	let password = request.body.password;
+	// Ensure the input fields exists and are not empty
+	if (username && password) {
+		// Execute SQL query that'll select the account from the database based on the specified username and password
+		connection.query('SELECT * FROM customers WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
+			// If there is an issue with the query, output the error
+			if (error) throw error;
+			// If the account exists
+			if (results.length > 0) {
+				// Authenticate the user
+				request.session.loggedin = true;
+				request.session.username = username;
+				// Redirect to home page
+				
+			} else {
+				response.send('Incorrect Username and/or Password!');
+			}			
+			response.end();
+		});
+	} else {
+		response.send('Please enter Username and Password!');
+		response.end();
+	}
+});
 
 app.use((req, res) => {
     res.status(404).render('pages/404');
