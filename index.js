1bb9393b3b67a614ca15a02ab67e62477afca83a
Andrew Sander <andrsand1@gmail.com>
2023-06-08 11:59:27 -0600

    commit message

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 const path = require('path');
 const mysql = require ('mysql');
 const session = require('express-session');
+const mysql = require ('mysql');
+const session = require('express-session');
 
 app.set('view engine', 'ejs');
 
@@ -11,14 +13,6 @@ app.listen(3000, (err) => {
     console.log('server is listening on port 3000');
 });
 
-function getDBH() {
-    return mysql.createConnection({
-        host: "localhost",
-        user: "HowardS",
-        password: "Password",
-        database: "travelexperts"
-    });
-}
 const connection = mysql.createConnection({
     host: "localhost",
     user: "Team5",
@@ -76,36 +70,40 @@ app.get('/welcome', (req, res) => {
 app.get('/login', (req, res) => {
     res.render('pages/login');
 });
+app.get('/account', (req, res) => {
+    console.log('account account')
+});
 app.post("/register", (req, res) => {
     console.log(req.body.fname + ", " + req.body.phone);
+
+
     res.redirect('/welcome');
 });
-// http://localhost:3000/auth
+
 app.post('/login', function(request, response) {
-	// Capture the input fields
+	
 	let username = request.body.username;
 	let password = request.body.password;
-	// Ensure the input fields exists and are not empty
+	
 	if (username && password) {
-		// Execute SQL query that'll select the account from the database based on the specified username and password
+		
 		connection.query('SELECT * FROM customers WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
-			// If there is an issue with the query, output the error
+			
 			if (error) throw error;
-			// If the account exists
+
 			if (results.length > 0) {
-				// Authenticate the user
+				
 				request.session.loggedin = true;
 				request.session.username = username;
-				// Redirect to home page
 				
+				response.redirect('/welcome');
 			} else {
-				response.send('Incorrect Username and/or Password!');
+				// popups.alert({
+                //     content: 'Incorrect Username or Password'
+                // });
 			}			
 			response.end();
 		});
-	} else {
-		response.send('Please enter Username and Password!');
-		response.end();
 	}
 });
 
